8dc8254acea44fb253c7cbf7958a90a67007b4ce
Michal Pruchniak <mpruchniak@gmail.com>
2017-03-19 19:41:35 +0100

    random

--- js/main.js
+++ js/main.js
@@ -6,16 +6,30 @@ var ctx = my_canvas.getContext('2d');
 
 var x = my_canvas.width/2;
 var y = my_canvas.height/2;
-var h = 40;
+var h = rand(10, 30);
+var r = rand(0, 255);
+var g = rand(0, 255);
+var b = rand(0, 255);
+var speedX = rand(1, 5);
+var speedY = rand(1, 5);
 var fps = 60;
 
-setInterval(animationLoop, 1000/fps);
-function animationLoop(){
-  if(x<my_canvas.width-h/2){
-    ctx.clearRect(0,0,my_canvas.width,my_canvas.height);
-      x += 1;
-      y += 1;
-      ctx.fillRect(x-h/2, y-h/2, h, h);
-      console.log(x);
+var lastTime = 0;
+// setInterval(animationLoop, 1000/fps);
+animationLoop();
+function animationLoop(time){
+  requestAnimationFrame(animationLoop);
+  if(x<my_canvas.width-h/2 && y<my_canvas.height-h/2){
+    if(time -lastTime >= 1000/fps){
+      lastTime = time;
+      ctx.clearRect(0,0,my_canvas.width,my_canvas.height);
+        x += speedX;
+        y += speedY;
+        ctx.fillStyle = "rgb("+r+","+g+","+b+")";
+        ctx.fillRect(x-h/2, y-h/2, h, h);
+    }
   }
+}
+function rand(min, max){
+  return Math.floor(Math.random()*(max-min+1)+min);
 }
